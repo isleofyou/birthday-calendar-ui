e605de80a287e2c1016c29b595d17ac20f8ca779
David Tran <frozenpho@gmail.com>
2021-12-15 09:36:15 -0700

    Initial commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { months } from '../../months_data';
-
+import Card from '../Card';
+import Form from '../Form';
 function App() {
 
   return (
     <div className="App">
       <h1>Birthdays</h1>
       <div className='bday-form'>
-
+        <Form />
       </div>
       <div className='bday-container'>
-
+        {months.forEach((month) => {
+          return <Card month={month.name}/>
+        })}
       </div>
     </div>
   );
---
+++ src/Components/Form.js
@@ -0,0 +1,32 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render () {
+      return (
+          <form className="addBirthday">
+              <label>
+                  Name:
+                  <input type="text" name="name" />
+              </label>
+              <label>
+                  Month:
+                  <input type="text" name="Month" />
+              </label>
+              <label>
+                  Day:
+                  <input type="text" name="day" />
+              </label>
+              <input type="submit" value="Add this birthday!" />
+          </form>
+      )
+    }
+}
+
+export default Form;
